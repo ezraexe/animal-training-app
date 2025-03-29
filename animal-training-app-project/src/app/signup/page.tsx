6bb5a813b5bcf353e98f6a29dba41c9215911789
ekim611 <ekim611@gatech.edu>
2025-03-29 17:58:18 -0400

    connect backend to signup page

--- animal-training-app-project/src/app/signup/page.tsx
+++ animal-training-app-project/src/app/signup/page.tsx
@@ -2,8 +2,69 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { isValidEmail, isValidPassword } from "@/lib/credentialAuth";
 
-export default function LoginPage() {
+export default function SignupPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [admin, setAdmin] = useState(false);
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError("");
+
+    if (!name) {
+      setError("Please enter your name");
+      return;
+    }
+
+    if (!isValidEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+
+    if (!isValidPassword(password)) {
+      setError("Please enter a valid password");
+      return;
+    }
+
+    if (password != confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await fetch("/api/user", {
+        // api/user is route for signup
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, password, admin }),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error);
+      }
+
+      router.push("/login");
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError("An unexpected error occurred");
+      }
+    }
+  };
+
+  // html code
   return (
     <div className="min-h-screen flex items-center justify-center bg-white -mt-[6.375rem]">
       {/* screen and red circle */}
@@ -18,25 +79,33 @@ export default function LoginPage() {
         </h2>
 
         {/* login form */}
-        <form className="mt-8">
+        <form className="mt-8" onSubmit={handleSignup}>
           <div className="space-y-4">
             <input
               type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               placeholder="Full Name"
               className="w-full px-1 py-1 border-b-[.156rem] border-[#D21312] placeholder-black focus:outline-none"
             />
             <input
               type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Email"
               className="w-full px-1 py-1 border-b-[.156rem] border-[#D21312] placeholder-black focus:outline-none"
             />
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
               className="w-full px-1 py-1 border-b-[.156rem] border-[#D21312] placeholder-black focus:outline-none"
             />
             <input
               type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
               placeholder="Confirm Password"
               className="w-full px-1 py-1 border-b-[.156rem] border-[#D21312] placeholder-black focus:outline-none"
             />
@@ -47,6 +116,8 @@ export default function LoginPage() {
             <input
               type="checkbox"
               id="admin"
+              checked={admin}
+              onChange={(e) => setAdmin(e.target.checked)}
               className="h-[25px] w-[25px] accent-[#D21312] cursor-pointer border border-[#D21312]"
             />
             <label
@@ -62,7 +133,7 @@ export default function LoginPage() {
           type="submit"
           className="w-full py-3 bg-[#D21312] rounded-2xl text-white text-2xl font-semibold focus:outline-none mt-14 cursor-pointer"
         >
-        	Sign Up
+          Sign Up
         </button>
 
         {/* sign up link */}
---
+++ animal-training-app-project/src/context/UserContext.tsx
@@ -0,0 +1,1 @@
+// for login authentication throughout the app 
