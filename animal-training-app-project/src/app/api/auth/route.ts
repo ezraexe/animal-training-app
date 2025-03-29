4f6a1fc812821be68f6ffcaab66759f77db297cf
ekim611 <ekim611@gatech.edu>
2025-03-29 15:21:32 -0400

    updated login api

--- animal-training-app-project/src/app/api/auth/route.ts
+++ animal-training-app-project/src/app/api/auth/route.ts
@@ -1,1 +1,19 @@
 // login api route
+import connectDb from "@/lib/database";
+import { NextRequest, NextResponse } from "next/server";
+import { User } from "@/schemas/user.schema";
+import bcrypt from "bcrypt";
+
+export async function POST(request: NextRequest) {
+  try {
+    await connectDb();
+    
+    
+  } catch (error) {
+    console.error("Error while trying to login", error);
+    return NextResponse.json(
+      { error: "Failed to login" },
+      { status: 500 }
+    );
+  }
+}
